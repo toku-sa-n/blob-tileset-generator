ccc855681feb640c44e5aa68d052184235e5595e
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 19:09:25 +0900

    Split a tile into 4

--- src/BlobTileGenerator.hs
+++ src/BlobTileGenerator.hs
@@ -8,19 +8,36 @@ module BlobTileGenerator
     , fromPartsUnchecked
     ) where
 
-import           Codec.Picture       (Image (Image, imageWidth),
+import           Codec.Picture       (Image (Image, imageHeight, imageWidth),
                                       Pixel (PixelBaseComponent))
 import           Codec.Picture.Extra (crop)
 import           Control.Lens        (makeLenses, (^.))
 import           Foreign             (Storable)
 
+data TileSplitIntoFourDirections a =
+    TileSplitIntoFourDirections
+        { _northWest :: Image a
+        , _northEast :: Image a
+        , _southWest :: Image a
+        , _southEast :: Image a
+        }
+
+makeLenses ''TileSplitIntoFourDirections
+
+instance (Eq (PixelBaseComponent a), Storable (PixelBaseComponent a)) =>
+         Eq (TileSplitIntoFourDirections a) where
+    t1 == t2 =
+        t1 ^. northWest == t2 ^. northWest &&
+        t1 ^. northEast == t2 ^. northEast &&
+        t1 ^. southWest == t2 ^. southWest && t1 ^. southEast == t2 ^. southEast
+
 data Tile1x5 a =
     Tile1x5
-        { _noConnection         :: Image a
-        , _verticalConnection   :: Image a
-        , _horizontalConnection :: Image a
-        , _innerCorner          :: Image a
-        , _allConnection        :: Image a
+        { _noConnection         :: TileSplitIntoFourDirections a
+        , _verticalConnection   :: TileSplitIntoFourDirections a
+        , _horizontalConnection :: TileSplitIntoFourDirections a
+        , _innerCorner          :: TileSplitIntoFourDirections a
+        , _allConnection        :: TileSplitIntoFourDirections a
         }
 
 makeLenses ''Tile1x5
@@ -38,7 +55,12 @@ splitImage :: Pixel a => Image a -> Maybe (Tile1x5 a)
 splitImage img
     | isCorrectSize img =
         Just $
-        Tile1x5 (head parts) (parts !! 1) (parts !! 2) (parts !! 3) (parts !! 4)
+        fromPartsUnchecked
+            (head parts)
+            (parts !! 1)
+            (parts !! 2)
+            (parts !! 3)
+            (parts !! 4)
     | otherwise = Nothing
   where
     parts = fmap (\y -> crop 0 y w w img) topYCoord
@@ -46,8 +68,34 @@ splitImage img
     w = imageWidth img
 
 fromPartsUnchecked ::
-       Image a -> Image a -> Image a -> Image a -> Image a -> Tile1x5 a
-fromPartsUnchecked = Tile1x5
+       Pixel a
+    => Image a
+    -> Image a
+    -> Image a
+    -> Image a
+    -> Image a
+    -> Tile1x5 a
+fromPartsUnchecked n v h i a = Tile1x5 n' v' h' i' a'
+  where
+    n' = tileSplitIntoFourDirections n
+    v' = tileSplitIntoFourDirections v
+    h' = tileSplitIntoFourDirections h
+    i' = tileSplitIntoFourDirections i
+    a' = tileSplitIntoFourDirections a
+
+tileSplitIntoFourDirections ::
+       Pixel a => Image a -> TileSplitIntoFourDirections a
+tileSplitIntoFourDirections img = TileSplitIntoFourDirections nw ne sw se
+  where
+    nw = cropToQuarter 0 0
+    ne = cropToQuarter 0 wHalf
+    sw = cropToQuarter hHalf 0
+    se = cropToQuarter hHalf wHalf
+    cropToQuarter x y = crop x y wHalf hHalf img
+    wHalf = w `div` 2
+    hHalf = h `div` 2
+    w = imageWidth img
+    h = imageHeight img
 
 isCorrectSize :: Image a -> Bool
 isCorrectSize img = isImage1x5Size img && isWidthEven img
