3f372862c52bd80f25ccfc566ea4c0841b2c91f9
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 17:51:16 +0900

    Define `splitImage`

--- src/BlobTileGenerator.hs
+++ src/BlobTileGenerator.hs
@@ -1,8 +1,53 @@
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module BlobTileGenerator
-    ( isCorrectSize
+    ( Tile1x5
+    , isCorrectSize
+    , splitImage
+    , fromPartsUnchecked
     ) where
 
-import           Codec.Picture (Image (Image))
+import           Codec.Picture       (Image (Image, imageWidth),
+                                      Pixel (PixelBaseComponent))
+import           Codec.Picture.Extra (crop)
+import           Control.Lens        (makeLenses, (^.))
+import           Foreign             (Storable)
+
+data Tile1x5 a =
+    Tile1x5
+        { _noConnection         :: Image a
+        , _verticalConnection   :: Image a
+        , _horizontalConnection :: Image a
+        , _innerCorner          :: Image a
+        , _allConnection        :: Image a
+        }
+
+makeLenses ''Tile1x5
+
+instance (Eq (PixelBaseComponent a), Storable (PixelBaseComponent a)) =>
+         Eq (Tile1x5 a) where
+    t1 == t2 =
+        t1 ^. noConnection == t2 ^. noConnection &&
+        t1 ^. verticalConnection == t2 ^. verticalConnection &&
+        t1 ^. horizontalConnection == t2 ^. horizontalConnection &&
+        t1 ^. innerCorner == t2 ^. innerCorner &&
+        t1 ^. allConnection == t2 ^. allConnection
+
+splitImage :: Pixel a => Image a -> Maybe (Tile1x5 a)
+splitImage img
+    | isCorrectSize img =
+        Just $
+        Tile1x5 (head parts) (parts !! 1) (parts !! 2) (parts !! 3) (parts !! 4)
+    | otherwise = Nothing
+  where
+    parts = fmap (\y -> crop 0 y w w img) topCoord
+    topCoord = take 5 [0,w ..]
+    w = imageWidth img
+
+fromPartsUnchecked ::
+       Image a -> Image a -> Image a -> Image a -> Image a -> Tile1x5 a
+fromPartsUnchecked = Tile1x5
 
 isCorrectSize :: Image a -> Bool
 isCorrectSize img = isImage1x5Size img && isWidthEven img
--- tests/BlobTileGeneratorSpec.hs
+++ tests/BlobTileGeneratorSpec.hs
@@ -2,12 +2,33 @@ module BlobTileGeneratorSpec
     ( spec
     ) where
 
-import           BlobTileGenerator (isCorrectSize)
-import           Codec.Picture     (Image, PixelRGB8 (PixelRGB8), generateImage)
-import           Test.Hspec        (Spec, describe, it, shouldBe)
+import           BlobTileGenerator   (fromPartsUnchecked, isCorrectSize,
+                                      splitImage)
+import           Codec.Picture       (Image, PixelRGB8 (PixelRGB8),
+                                      generateImage)
+import           Codec.Picture.Extra (below)
+import           Test.Hspec          (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = testIsCorrectSize
+spec = do
+    testIsCorrectSize
+    testSplitImage
+
+testSplitImage :: Spec
+testSplitImage =
+    describe "tile1x5" $
+    it "returns a Just value including the Tile1x5 type value containing images separated into 5. if the image has a correct size." $
+    splitImage correctTileFile == Just expected
+  where
+    correctTileFile =
+        below [redTile, blueTile, greenTile, yellowTile, blackTile]
+    expected =
+        fromPartsUnchecked redTile blueTile greenTile yellowTile blackTile
+    redTile = generateImage (\_ _ -> PixelRGB8 255 0 0) 2 2
+    blueTile = generateImage (\_ _ -> PixelRGB8 0 0 255) 2 2
+    greenTile = generateImage (\_ _ -> PixelRGB8 0 255 0) 2 2
+    yellowTile = generateImage (\_ _ -> PixelRGB8 255 255 0) 2 2
+    blackTile = generateImage (\_ _ -> PixelRGB8 0 0 0) 2 2
 
 testIsCorrectSize :: Spec
 testIsCorrectSize =
