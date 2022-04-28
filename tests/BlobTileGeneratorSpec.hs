390b4cd9c91847e1f949fe77a35b31c2af185c8a
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 16:00:13 +0900

    Test if the function returns `False` correctly

--- tests/BlobTileGeneratorSpec.hs
+++ tests/BlobTileGeneratorSpec.hs
@@ -8,8 +8,10 @@ import           Test.Hspec        (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec =
-    describe "isImage1x5Size" $
-    it "returns True if the given image has a width-to-height ratio of 1:5." $
-    isImage1x5Size image `shouldBe` True
+    describe "isImage1x5Size" $ do
+        it "returns True if the given image has a width-to-height ratio of 1:5." $
+            isImage1x5Size image `shouldBe` True
+        it "returns False otherwise" $ isImage1x5Size image2 `shouldBe` False
   where
     image = generateImage (\_ _ -> PixelRGB8 0 0 0) 1 5
+    image2 = generateImage (\_ _ -> PixelRGB8 0 0 0) 2 5
