0d8e8cb88f9be02f1f095c3a2ede1ba668ce238b
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 16:33:07 +0900

    Hide unnecessary functions

--- src/BlobTileGenerator.hs
+++ src/BlobTileGenerator.hs
@@ -1,8 +1,5 @@
 module BlobTileGenerator
     ( isCorrectSize
-    , isImage1x5Size
-    , isWidthMoreThanOrEqualTo4
-    , isWidthEven
     ) where
 
 import           Codec.Picture (Image (Image))
@@ -12,9 +9,6 @@ isCorrectSize img = isImage1x5Size img && isWidthEven img
 
 isImage1x5Size :: Image a -> Bool
 isImage1x5Size (Image w h _) = w * 5 == h
-
-isWidthMoreThanOrEqualTo4 :: Image a -> Bool
-isWidthMoreThanOrEqualTo4 (Image w _ _) = w >= 4
 
 isWidthEven :: Image a -> Bool
 isWidthEven (Image w _ _) = even w
--- tests/BlobTileGeneratorSpec.hs
+++ tests/BlobTileGeneratorSpec.hs
@@ -2,17 +2,12 @@ module BlobTileGeneratorSpec
     ( spec
     ) where
 
-import           BlobTileGenerator (isCorrectSize, isImage1x5Size, isWidthEven,
-                                    isWidthMoreThanOrEqualTo4)
+import           BlobTileGenerator (isCorrectSize)
 import           Codec.Picture     (Image, PixelRGB8 (PixelRGB8), generateImage)
 import           Test.Hspec        (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = do
-    testIsCorrectSize
-    testIsImage1x5Size
-    testIsWidthMoreThanOrEqualTo4
-    testIsWidthEven
+spec = testIsCorrectSize
 
 testIsCorrectSize :: Spec
 testIsCorrectSize =
@@ -25,39 +20,6 @@ testIsCorrectSize =
     correctSize = generateBlackImage 2 10
     oddWidth = generateBlackImage 1 5
     incorrectRatio = generateBlackImage 2 9
-
-testIsImage1x5Size :: Spec
-testIsImage1x5Size =
-    describe "isImage1x5Size" $ do
-        it "returns True if the given image has a width-to-height ratio of 1:5." $
-            isImage1x5Size correctRatio `shouldBe` True
-        it "returns False otherwise" $
-            isImage1x5Size incorrectRatio `shouldBe` False
-  where
-    correctRatio = generateBlackImage 1 5
-    incorrectRatio = generateBlackImage 2 5
-
-testIsWidthMoreThanOrEqualTo4 :: Spec
-testIsWidthMoreThanOrEqualTo4 =
-    describe "isWidthMoreThanOrEqualTo4" $ do
-        it
-            "returns True if the width of the given image is more than or equal to 4." $
-            isWidthMoreThanOrEqualTo4 enoughWidth `shouldBe` True
-        it "returns False otherwise" $
-            isWidthMoreThanOrEqualTo4 notEnoughWidth `shouldBe` False
-  where
-    enoughWidth = generateBlackImage 5 1
-    notEnoughWidth = generateBlackImage 3 8
-
-testIsWidthEven :: Spec
-testIsWidthEven =
-    describe "isWidthEven" $ do
-        it "returns True if the width of the given image is even." $
-            isWidthEven correctWidth `shouldBe` True
-        it "returns False otherwise" $ isWidthEven evenWidth `shouldBe` False
-  where
-    correctWidth = generateBlackImage 4 1
-    evenWidth = generateBlackImage 1 1
 
 generateBlackImage :: Int -> Int -> Image PixelRGB8
 generateBlackImage = generateImage (\_ _ -> PixelRGB8 0 0 0)
