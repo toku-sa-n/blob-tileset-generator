ef8d33a92545ed95a919b870cf15060147409039
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 16:12:45 +0900

    Define `isWidthMoreThanOrEqualTo4`
    We cannot split an image into 4 if its width is less than 4.

--- src/BlobTileGenerator.hs
+++ src/BlobTileGenerator.hs
@@ -1,8 +1,12 @@
 module BlobTileGenerator
     ( isImage1x5Size
+    , isWidthMoreThanOrEqualTo4
     ) where
 
 import           Codec.Picture (Image (Image))
 
 isImage1x5Size :: Image a -> Bool
 isImage1x5Size (Image w h _) = w * 5 == h
+
+isWidthMoreThanOrEqualTo4 :: Image a -> Bool
+isWidthMoreThanOrEqualTo4 (Image w _ _) = w >= 4
--- tests/BlobTileGeneratorSpec.hs
+++ tests/BlobTileGeneratorSpec.hs
@@ -2,18 +2,23 @@ module BlobTileGeneratorSpec
     ( spec
     ) where
 
-import           BlobTileGenerator (isImage1x5Size)
+import           BlobTileGenerator (isImage1x5Size, isWidthMoreThanOrEqualTo4)
 import           Codec.Picture     (Image, PixelRGB8 (PixelRGB8), generateImage)
 import           Test.Hspec        (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec =
+spec = do
     describe "isImage1x5Size" $ do
         it "returns True if the given image has a width-to-height ratio of 1:5." $
             isImage1x5Size correctRatio `shouldBe` True
         it "returns False otherwise" $
             isImage1x5Size incorrectRatio `shouldBe` False
+    describe "isWidthMoreThanOrEqualTo4" $
+        it
+            "returns True if the width of the given image is more than or equal to 4." $
+        isWidthMoreThanOrEqualTo4 enoughWidth `shouldBe` True
   where
+    enoughWidth = generateBlackImage 5 1
     correctRatio = generateBlackImage 1 5
     incorrectRatio = generateBlackImage 2 5
 
