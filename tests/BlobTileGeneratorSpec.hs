744e11cd168475cb56a6133bb684f112a4426eab
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-28 16:02:28 +0900

    Rename to `(in)?correctRatio`

--- tests/BlobTileGeneratorSpec.hs
+++ tests/BlobTileGeneratorSpec.hs
@@ -10,11 +10,12 @@ spec :: Spec
 spec =
     describe "isImage1x5Size" $ do
         it "returns True if the given image has a width-to-height ratio of 1:5." $
-            isImage1x5Size image `shouldBe` True
-        it "returns False otherwise" $ isImage1x5Size image2 `shouldBe` False
+            isImage1x5Size correctRatio `shouldBe` True
+        it "returns False otherwise" $
+            isImage1x5Size incorrectRatio `shouldBe` False
   where
-    image = generateBlackImage 1 5
-    image2 = generateBlackImage 2 5
+    correctRatio = generateBlackImage 1 5
+    incorrectRatio = generateBlackImage 2 5
 
 generateBlackImage :: Int -> Int -> Image PixelRGB8
 generateBlackImage = generateImage (\_ _ -> PixelRGB8 0 0 0)
