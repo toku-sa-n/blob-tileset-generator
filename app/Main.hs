928d84aa2a5746e195ce3a4c9f9bcce942824528
Hiroki Tokunaga <tokusan441@gmail.com>
2022-05-01 20:15:24 +0900

    Split the big main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,43 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Main
     ( main
     ) where
 
-import           BlobTileGenerator  (generateBlobTile)
-import           Codec.Picture      (convertRGBA8, readImage, writePng)
-import           System.Environment (getArgs)
-import           System.Exit        (ExitCode (ExitFailure), exitWith)
-import           System.IO          (hPutStrLn, stderr)
+import           BlobTileGenerator          (generateBlobTile)
+import           Codec.Picture              (Image, Pixel, PixelRGBA8,
+                                             convertRGBA8, readImage, writePng)
+import           Control.Monad.Trans.Except (ExceptT (ExceptT), runExceptT)
+import           Data.Either.Combinators    (maybeToRight)
+import           System.Environment         (getArgs)
+import           System.Exit                (ExitCode (ExitFailure), exitWith)
+import           System.IO                  (hPutStrLn, stderr)
 
 main :: IO ()
-main = do
-    args <- getArgs
-    if null args
-        then exit "Specify a 1x5 tile image file."
-        else do
-            img <- readImage $ head args
-            case img of
-                Right x ->
-                    case generateBlobTile $ convertRGBA8 x of
-                        Just y -> writePng "blob_tile.png" y
-                        Nothing ->
-                            exit
-                                "Failed to convert the tile image. Please check the image's size."
-                Left e -> exit $ "Failed to load an image: " <> e
+main =
+    runExceptT runOrErr >>= \case
+        Right () -> return ()
+        Left e   -> exitWithErrMsg e
 
-exit :: String -> IO ()
-exit msg = hPutStrLn stderr msg >> exitWith (ExitFailure 1)
+runOrErr :: ExceptT String IO ()
+runOrErr =
+    getArgsOrErr >>= readImageOrErr . head >>= generateBlobTileOrErr >>=
+    ExceptT . fmap Right . writePng "blob_tile.png"
+
+getArgsOrErr :: ExceptT String IO [String]
+getArgsOrErr = do
+    args <- ExceptT $ fmap Right getArgs
+    if null args
+        then ExceptT $ return $ Left "Specify a 1x5 tile image file path."
+        else ExceptT $ return $ Right args
+
+readImageOrErr :: FilePath -> ExceptT String IO (Image PixelRGBA8)
+readImageOrErr path = ExceptT $ fmap convertRGBA8 <$> readImage path
+
+generateBlobTileOrErr :: Pixel a => Image a -> ExceptT String IO (Image a)
+generateBlobTileOrErr = ExceptT . return . maybeToRight msg . generateBlobTile
+  where
+    msg = "Failed to convert the tile image. Please check the image's size."
+
+exitWithErrMsg :: String -> IO ()
+exitWithErrMsg msg = hPutStrLn stderr msg >> exitWith (ExitFailure 1)
